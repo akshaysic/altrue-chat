fbe9de9bc7ed4df993e0c18192a2afadba908738
akshaysic <akshaysic@gmail.com>
2025-07-10 19:44:47 +0200

    wrote a basic flask check

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
-git remote add origin https://github.com/akshaysic/altrue-chat.git
-git branch -M main
-git push -u origin main
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
+
+def create_app():
+    app = Flask(__name__)
+
+    # Root route
+    @app.route('/')
+    def home():
+        return "API working ✅"
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
